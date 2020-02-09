02207951d53471f0d2f90a6e808312dd962400de
fachsimpeln <35078533+fachsimpeln@users.noreply.github.com>
2020-02-09 21:46:32 +0100

    Codeform as include for g/index.php

---
+++ inc/codeform.inc.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="de" dir="ltr">
+     <head>
+          <meta charset="utf-8">
+          <title>mindthecode.</title>
+
+          <meta name="author" content="fachsimpeln">
+          <meta name="publisher" content="fachsimpeln">
+          <meta name="description" content="">
+
+          <meta name="page-topic" content="Medien">
+          <meta name="page-type" content="Produktinfo">
+          <meta name="audience" content="Alle">
+          <meta http-equiv="content-language" content="de">
+          <meta name="robots" content="index, nofollow">
+
+
+          <link rel="stylesheet" href="../css/master.css">
+
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+     </head>
+     <body>
+
+          <div class="navbar">
+               <ul>
+                    <li>
+                         <img src="../assets/img/mindthecode-logo.png" alt="MindTheCode-Logo">
+                    </li>
+                    <li>
+                         mindthecode.
+                    </li>
+                    <li>
+                         <img src="../assets/icons/add_box-24px.svg" alt="">
+                         ERSTELLEN
+                    </li>
+               </ul>
+          </div>
+
+          <form class="codeform" action="" method="post">
+               <label for="code">Code:</label>
+               <input id="code" type="text" name="c" value="">
+               <input type="submit" name="submit" value="Quiz öffnen">
+          </form>
+
+          <footer>
+               <ul>
+                    <li>copyright fachsimpeln 2020</li>
+                    <li>Impressum</li>
+                    <li>Datenschutz</li>
+               </ul>
+          </footer>
+
+     </body>
+     </html>
